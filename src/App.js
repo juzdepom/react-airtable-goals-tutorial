a21de022dd0dd08701a37a59b67d136fbb727def
Julia Gao Miller <julia.gao.miller@gmail.com>
2021-11-12 07:39:48 +0700

    successfully pulling data from airtable

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Airtable from "airtable";
+import { baseId, apiKey } from './airtableIds'
+
+
+// base id appqfpGEd5sP5aLXx
+const base = new Airtable({ apiKey: apiKey }).base(baseId);
 
 function App() {
+
+  //assign the data to states
+  const [goals, setGoals] = useState([]);
+  const [updates, setUpdates] = useState([]);
+
+  //this method pulls in the data
+  useEffect(() => {
+    //pull in the 'goals' table view data
+    base("goals")
+      .select({ view: "Grid view" })
+      .eachPage((records, fetchNextPage) => { 
+        // console.log(records)
+        setGoals(records);
+        fetchNextPage();
+      });
+    //pull in the 'updates' table view data
+    base("updates")
+      .select({ view: "Grid view" })
+      .eachPage((records, fetchNextPage) => {
+        setUpdates(records);
+        fetchNextPage();
+      });
+  }, []);
+
   return (
     <div>
     </div>
